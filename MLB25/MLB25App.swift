66341b7113c7411e52e4a964890bda16a32e7908
Jake Abramson <abramso@bc.edu>
2026-04-01 17:45:01 -0400

    Initial Commit

---
+++ MLB25/MLB25App.swift
@@ -0,0 +1,17 @@
+//
+//  MLB25App.swift
+//  MLB25
+//
+//  Created by Jake Abramson on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MLB25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
